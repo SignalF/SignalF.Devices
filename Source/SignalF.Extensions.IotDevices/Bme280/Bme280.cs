b3753736a17181afb475c4cd454c2bc687e626fb
Olaf Meyer <olaf.meyer@e-ypsilon.d>
2024-07-30 18:58:30 +0200

    Added device base classes. Added BME280, BME680, and BMP280.

---
+++ Source/SignalF.Extensions.IotDevices/Bme280/Bme280.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Extensions.Logging;
+using SignalF.Controller.Hardware.Channels;
+using SignalF.Controller.Hardware.Channels.I2c;
+using SignalF.Controller.Signals;
+using SignalF.Datamodel.Hardware;
+
+namespace SignalF.Extensions.IotDevices.Bme280;
+
+
+public class Bme280 : I2cIotDevice
+{
+    private const int TemperatureIndex = 0;
+    private const int PressureIndex = 1;
+    private const int HumidityIndex = 2;
+
+    private readonly int[] _signalIndices = new int[3];
+
+    private Iot.Device.Bmxx80.Bme280? _bme280;
+
+    public Bme280(ISignalHub signalHub, ILogger<I2cIotDevice> logger) : base(signalHub, logger)
+    {
+    }
+
+    public override void AssignChannels(IList<IChannel> channels)
+    {
+        _bme280 = new Iot.Device.Bmxx80.Bme280(new I2cIotDeviceConnector(channels.OfType<II2cChannel>().ToList()));
+    }
+
+    protected override void OnConfigure(IDeviceConfiguration configuration)
+    {
+        base.OnConfigure(configuration);
+
+        Array.Fill(_signalIndices, -1);
+        foreach (var signalDefinition in configuration.Definition.Template.SignalSourceDefinitions)
+        {
+            var signalConfiguration = configuration.SignalSources.Single(s => s.Definition.Id == signalDefinition.Id);
+
+            switch (signalDefinition.Name)
+            {
+                case "Temperature":
+                    {
+                        _signalIndices[TemperatureIndex] = GetSignalIndex(signalConfiguration);
+                        break;
+                    }
+                case "Pressure":
+                    {
+                        _signalIndices[PressureIndex] = GetSignalIndex(signalConfiguration);
+                        break;
+                    }
+                case "Humidity":
+                    {
+                        _signalIndices[HumidityIndex] = GetSignalIndex(signalConfiguration);
+                        break;
+                    }
+                default:
+                    {
+                        throw new Exception($"Configuration of device BME280 is wrong! Invalid signal definition name '{signalDefinition.Name}'.");
+                    }
+            }
+        }
+    }
+
+    protected override void OnRead()
+    {
+        var timestamp = SignalHub.GetTimestamp();
+
+        if (_bme280 == null)
+        {
+            return;
+        }
+
+        if (_signalIndices[TemperatureIndex] != -1)
+        {
+            _bme280.TryReadTemperature(out var temperature);
+            SignalSources[_signalIndices[TemperatureIndex]].AssignWith(temperature.DegreesCelsius, timestamp);
+        }
+        if (_signalIndices[TemperatureIndex] != -1)
+        {
+            _bme280.TryReadPressure(out var pressure);
+            SignalSources[_signalIndices[PressureIndex]].AssignWith(pressure.Pascals, timestamp);
+        }
+        if (_signalIndices[TemperatureIndex] != -1)
+        {
+            _bme280.TryReadHumidity(out var humidity);
+            SignalSources[_signalIndices[HumidityIndex]].AssignWith(humidity.Percent, timestamp);
+        }
+    }
+}
---
+++ Source/SignalF.Extensions.IotDevices/Bme680/Bme680.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Extensions.Logging;
+using SignalF.Controller.Hardware.Channels.I2c;
+using SignalF.Controller.Hardware.Channels;
+using SignalF.Controller.Signals;
+using SignalF.Datamodel.Hardware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalF.Extensions.IotDevices.Bme680
+{
+    public class Bme680 : I2cIotDevice
+    {
+        private const int TemperatureIndex = 0;
+        private const int PressureIndex = 1;
+        private const int HumidityIndex = 2;
+        private const int GasResistanceIndex = 3;
+
+        private readonly int[] _signalIndices = new int[4];
+
+        private Iot.Device.Bmxx80.Bme680? _bme680;
+
+        public Bme680(ISignalHub signalHub, ILogger<I2cIotDevice> logger) : base(signalHub, logger)
+        {
+        }
+
+        public override void AssignChannels(IList<IChannel> channels)
+        {
+            _bme680 = new Iot.Device.Bmxx80.Bme680(new I2cIotDeviceConnector(channels.OfType<II2cChannel>().ToList()));
+        }
+
+        protected override void OnConfigure(IDeviceConfiguration configuration)
+        {
+            base.OnConfigure(configuration);
+
+            Array.Fill(_signalIndices, -1);
+            foreach (var signalDefinition in configuration.Definition.Template.SignalSourceDefinitions)
+            {
+                var signalConfiguration = configuration.SignalSources.Single(s => s.Definition.Id == signalDefinition.Id);
+
+                switch (signalDefinition.Name)
+                {
+                    case "Temperature":
+                        {
+                            _signalIndices[TemperatureIndex] = GetSignalIndex(signalConfiguration);
+                            break;
+                        }
+                    case "Pressure":
+                        {
+                            _signalIndices[PressureIndex] = GetSignalIndex(signalConfiguration);
+                            break;
+                        }
+                    case "Humidity":
+                        {
+                            _signalIndices[HumidityIndex] = GetSignalIndex(signalConfiguration);
+                            break;
+                        }
+                    case "GasResistance":
+                        {
+                            _signalIndices[GasResistanceIndex] = GetSignalIndex(signalConfiguration);
+                            break;
+                        }
+                    default:
+                        {
+                            throw new Exception($"Configuration of device BME680 is wrong! Invalid signal definition name '{signalDefinition.Name}'.");
+                        }
+                }
+            }
+        }
+
+        protected override void OnRead()
+        {
+            var timestamp = SignalHub.GetTimestamp();
+
+            if (_bme680 == null)
+            {
+                return;
+            }
+
+            if (_signalIndices[TemperatureIndex] != -1)
+            {
+                _bme680.TryReadTemperature(out var temperature);
+                SignalSources[_signalIndices[TemperatureIndex]].AssignWith(temperature.DegreesCelsius, timestamp);
+            }
+            if (_signalIndices[TemperatureIndex] != -1)
+            {
+                _bme680.TryReadPressure(out var pressure);
+                SignalSources[_signalIndices[PressureIndex]].AssignWith(pressure.Pascals, timestamp);
+            }
+            if (_signalIndices[TemperatureIndex] != -1)
+            {
+                _bme680.TryReadHumidity(out var humidity);
+                SignalSources[_signalIndices[HumidityIndex]].AssignWith(humidity.Percent, timestamp);
+            }
+            if (_signalIndices[TemperatureIndex] != -1)
+            {
+                _bme680.TryReadGasResistance(out var gasResistance);
+                SignalSources[_signalIndices[GasResistanceIndex]].AssignWith(gasResistance.Ohms, timestamp);
+            }
+        }
+    }
+}
