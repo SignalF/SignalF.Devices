8d711d5afc6d51229162d3b6a73801e0eb0abbfc
Olaf Meyer <olaf.meyer@scotec-software.com>
2024-08-03 12:02:18 +0200

    Assigned Device attribute.

--- Source/SignalF.Devices.CpuTemperature/CpuTemperature.cs
+++ Source/SignalF.Devices.CpuTemperature/CpuTemperature.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.Versioning;
 using Microsoft.Extensions.Logging;
 using Scotec.Math.Units;
+using SignalF.Configuration;
 using SignalF.Controller.Signals;
 using SignalF.Controller.Signals.Devices;
 using SignalF.Datamodel.Hardware;
@@ -11,6 +12,7 @@ namespace SignalF.Devices.CpuTemperature;
 ///     CPU temperature.
 /// </summary>
 [SupportedOSPlatform("linux")]
+[Device]
 public sealed class CpuTemperature : NullDevice<IDeviceConfiguration>, ICpuTemperature
 {
     private int _signalIndex;
